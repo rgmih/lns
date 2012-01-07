bad4dbc953eb8f00306941c8b0e455aca20ee3a4
vadmak <vadmak@vadmak-laptop.dir320>
2012-01-07 22:25:12 +0400

    Printing out parsed JSON string as is

--- static/index.js
+++ static/index.js
@@ -1,13 +1,29 @@
+_on_ajax_success = function(data) {
+	var parsed_data = jQuery.parseJSON(data);
+	var container = document.getElementById('container');
+	while (container.children.length > 0) {
+		container.removeChild(container.firstChild);
+	}
+	
+	for (host in parsed_data) {
+		var files = parsed_data[host];
+		for (file in files) {
+			var isdir = files[file]['isdir'];
+			var size = files[file]['size'];
 
+			var new_element = document.createElement('a');
+			new_element.textContent = host.toString() + ':  ' + file.toString() + 
+				',  directory: ' + isdir.toString() + ',  size: ' + size.toString() + ' bytes';
+			container.appendChild(new_element);
+			container.appendChild(document.createElement('br'));
+		}
+	}
+	
+}
 
 update_entries = function() {
-	var response_data = new String();
 	$.get(
 		"/ls",
-		function(data) {
-			response_data += data.toString();
-		}
+		_on_ajax_success
 	);
-	
-	alert('Refreshed. Response data: ' + response_data);
 };
